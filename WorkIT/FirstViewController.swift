9c7768a3e527676d1a0c1d2c9683c4ee83141978
Pavlina Koleva <p.kolevaa@gmail.com>
2017-01-24 21:09:41 +0200

    Initial Commit

---
+++ WorkIT/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WorkIT
+//
+//  Created by Pavlina Koleva on 1/24/17.
+//  Copyright © 2017 Pavlina Koleva. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
